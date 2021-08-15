cd0ddcc7185825933b9701570063cce04e5b5813
QuangTran <trantran30102002@gmail.com>
2021-08-15 20:31:47 +0700

    finish header and footer

--- js/header-js.js
+++ js/header-js.js
@@ -4,7 +4,7 @@
             $(".search-click").click(function () {
                 $(this).parents(".header") 
                 // $(".search").slideToggle();
-                $(".search").addClass("open");
+                $(".search").toggleClass("open");
 
                 // $(this).parents("body").toggleClass("is-fixed");
             });
@@ -16,22 +16,22 @@
             /////////////////////////////
             $(".popular").click(function () {
                 $(this).parents(".popular")
-                $(".popular__heart").addClass("open-1")
-                $(".dark").addClass("open-1")
-                $(".selec").addClass("open-1");
+                $(".popular__heart").toggleClass("open-1")
+                $(".dark").toggleClass("open-1")
+                $(".popular").toggleClass("zindex");
             });
             ////////////////////////////
             $(".bag").click(function () {
                 $(this).parents(".bag")
-                $(".bag__heart").addClass("open-1")
-                $(".select").addClass("open-1")
-                $(".dark").addClass("open-1");
+                $(".bag__heart").toggleClass("open-1")
+                $(".dark").toggleClass("open-1")
+                $(".bag").toggleClass("zindex");
             });
             ///////////////////////////
             $(".signin-click").click(function() {
                 $(this).parents(".header")
-                $(".signin").addClass("s-open")
-                $(".dark").addClass("open-1");
+                $(".signin").toggleClass("s-open")
+                $(".dark").toggleClass("open-1");
             });
 
             $(".close-click").click(function() {
@@ -40,14 +40,35 @@
                 .removeClass("s-open")
                 $(".dark").removeClass("open-1");;
             });
-            ///////////////////////////
-            $(".dark").click(function() {
-                $(this).parents(".header")
-                $(".dark").removeClass();
-            });
+            // ///////////////////////////
+            // $(".dark").click(function() {
+            //     $(this).parents(".header")
+            //     $(".dark").removeClass();
+            // });
+
 
             // $(".popular").click(function () {
             //     $(this).parents(".popular")
             //     $(".popular__heart").removeClass("open-1");
             // });
+            ///////////////////////////////////
+            $(".bar-menu").click(function() {
+                $(this).parents(".header")
+                .find(".header__nav").toggleClass("h-open");
+                // $(this).parents("body").toggleClass("is-fixed");
+            });
+
+            $(".drop-link").click(function() {
+                $(this).parents(".header__list")
+                .find(".header__drop").toggleClass("h-open");
+            });
+            $(".drop-link").click(function() {
+                $(this).parents(".page")
+                .find(".page__drop").toggleClass("h-open");
+            });
+            //////////////////
+            $(".page__title").click(function(){
+                $(this).parents(".page__detail")
+                $(".page__detail").toggleClass("h-open");
+            });
     });
