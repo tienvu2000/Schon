94f102ce5242b51a6d28aa6cd225e1071f9b00ed
Vu Tien <tienxmin0123@gmail.com>
2021-09-08 04:12:10 +0700

    Update last time

--- js/main.js
+++ js/main.js
@@ -1,101 +1,66 @@
-// <!-- begin js back to top -->
-    $(document).ready(function() {
-        $(window).scroll(function() {
-            if($(window).scrollTop() > 300){
-                $('.btn__back-to-top').css({
-                    'opacity':'1','pointer-events':"auto",
-                });
-            } else {
-                $('.btn__back-to-top').css({
-                    'opacity':'0','pointer-events':"none"
-                });
-            }
-        });
-        $('.btn__back-to-top').click(function(){
-            $('html').animate({
-                scrollTop:0
-            },300);
-        })
-    }
-);
+const _$ = document.querySelector.bind(document)
+const _$$ = document.querySelectorAll.bind(document)
 
-// <!-- end back to top -->
-// <!-- // slick slider -->
-    $('.slider').slick({
-        dots: true,
-        infinite: true,
-        speed: 1000,
-        fade: true,
-        cssEase: 'linear'
-    }
-);
-{/* //  slick best seller */}
-    $('.product__seller-row').slick({
-        dots: true,
-        // autoplay: true,
-        // autoplaySpeed: 1500,
-        speed: 400,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        responsive: [
-            {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: true
-            }
-            },
-            {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1
-            }
-            },
-            {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1
-            }
-        }
-    ]
-});
+
+// // click next/prev btn
+
+// const nextBtn = _$('.next-btn')
+// const prevBtn = _$('.next-btn')
+// listItem = _$$('.product__item')
+// const index = 0
+// listItem.forEach((item) => {
+//     const itemIndex = item.getAttribute('data-index')
+//     if (Number(itemIndex) === this.index) {
+//       item.classList.add('active');
+//     }
+//   });
+// nextBtn.onclick = function(){
+//     if(itemIndex){
+//         index++;
+//     }
+// }
+// prevBtn.onclick = function(){
+//     if(itemIndex){
+//         index--;
+//     }
+// }
+
+// //  end click next/prev btn
+
+
 // <!-- js modal notify -->
-    const modalNotify = document.querySelector('.modal__notify');
-    const modalNotifyClose = document.querySelector('.modal__notify-btn-close');
-    const modalNotifyContainer = document.querySelector('.modal__notify-container');
-    function hideNotify(){
-        modalNotify.classList.remove('isShoww');
-    }
-    modalNotifyClose.addEventListener('click', hideNotify);
+const modalNotify = document.querySelector('.modal__notify');
+const modalNotifyClose = document.querySelector('.modal__notify-btn-close');
+const modalNotifyContainer = document.querySelector('.modal__notify-container');
+function hideNotify(){
+    modalNotify.classList.remove('isShoww');
+}
+modalNotifyClose.addEventListener('click', hideNotify);
 
-    modalNotify.addEventListener('click', hideNotify);
-    modalNotifyContainer.addEventListener('click' , function (event){
-    event.stopPropagation()
-    });
+modalNotify.addEventListener('click', hideNotify);
+modalNotifyContainer.addEventListener('click' , function (event){
+event.stopPropagation()
+});
 
 // <!-- end modal notify -->
 // Reveal Website 
-    window.addEventListener('scroll',reveal);
+window.addEventListener('scroll',reveal);
 
-    function reveal(){
-        var reveals = document.querySelectorAll('.reveal');
-        var revealLeft = document.querySelector('.reveal-left');
-        var revealRight = document.querySelectorAll('.reveal-right');
-        for(var i=0; i < reveals.length; i++){
-            var windowHeight = window.innerHeight;
-            var reveal = reveals[i].getBoundingClientRect().top;
-            var revealPoint = 100;
-            if(reveal < windowHeight - revealPoint){
-                reveals[i].classList.add('isActive');
-            }
-            else {
-                reveals[i].classList.remove('isActive');
-            }
+function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+    var revealLeft = document.querySelector('.reveal-left');
+    var revealRight = document.querySelectorAll('.reveal-right');
+    for(var i=0; i < reveals.length; i++){
+        var windowHeight = window.innerHeight;
+        var reveal = reveals[i].getBoundingClientRect().top;
+        var revealPoint = 100;
+        if(reveal < windowHeight - revealPoint){
+            reveals[i].classList.add('isActive');
         }
+        else {
+            reveals[i].classList.remove('isActive');
+        }
+    }
         // var revealPositionLeft = revealLeft.getBoundingClientRect().top;
         // if(revealPositionLeft < windownHeight - revealPoint){
         //     revealLeft.classList.add('isActiveLeft');
@@ -103,16 +68,16 @@
         // else {
         //     revealLeft.classList.remove('isActiveLeft');
         // }
-        for(var j=0; j < revealRight.length; j++){
-            var revealScrollRight = revealRight[j].getBoundingClientRect().top;
-            if(revealScrollRight < windowHeight - revealPoint){
-                revealRight[j].classList.add('isActiveLeft');
-            }
-            else {
-                revealRight[j].classList.remove('isActiveLeft');
-            }
+    for(var j=0; j < revealRight.length; j++){
+        var revealScrollRight = revealRight[j].getBoundingClientRect().top;
+        if(revealScrollRight < windowHeight - revealPoint){
+            revealRight[j].classList.add('isActiveLeft');
+        }
+        else {
+            revealRight[j].classList.remove('isActiveLeft');
         }
     }
+}
 // product next page
 const filterOptions = document.querySelectorAll(".product__filter-options");
 const productOptions = document.querySelectorAll(".product__row");
